950e808e82dfce189dfc2bfb58a5e477ac23d3a3
Tamara1406 <tamaram0053@gmail.com>
2023-06-27 15:29:53 +0200

    Kreirana Login stranica

---
+++ src/components/Login.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios';
+
+const Login = () => {
+
+    const[userData, setUserData] = useState({
+        email : "",
+        lozinka : ""
+    });
+
+    function handleInput(e){
+        let newUserData = userData;
+        newUserData[e.target.name] = e.target.value;
+        setUserData(newUserData);
+        console.log(userData)
+    }
+
+    function handleLogin(e){
+        e.preventDefault(); 
+        //axios
+        //.post("http://127.0.0.1:8000/api/login", userData)
+        //.then((odg) => {
+        //    console.log(odg.data);
+        //})
+        //.catch((e) => {
+        //    console.log(e);
+        //});
+    }
+
+  return (
+    <section
+      className="vh-100"
+      style={{
+        paddingTop: 4.5 + "rem",
+      }}
+    >
+      <div className="container-fluid h-custom">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          
+          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <form onSubmit={handleLogin}>
+              <div className="form-outline mb-4">
+                <input
+                  type="email"
+                  id="form3Example3"
+                  className="form-control form-control-lg"
+                  placeholder="Enter a valid email address"
+                  name="email"
+                  onInput={handleInput}
+                />
+                <label className="form-label" htmlFor="form3Example3">
+                  Email adresa
+                </label>
+              </div>
+
+              <div className="form-outline mb-3">
+                <input
+                  type="password"
+                  id="form3Example4"
+                  className="form-control form-control-lg"
+                  placeholder="Enter password"
+                  name="lozinka"
+                  onInput={handleInput}
+                />
+                <label className="form-label" htmlFor="form3Example4">
+                  Lozinka
+                </label>
+              </div>
+
+              <div className="text-center text-lg-start mt-4 pt-2">
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-lg"
+                  style={{
+                    paddingLeft: 2.5 + "rem",
+                    paddingRight: 2.5 + "rem",
+                  }}
+                >
+                  Prijavi se
+                </button>
+                <p className="small fw-bold mt-2 pt-1 mb-0">
+                  Nemaš svoj nalog?{" "}
+                  <a href="/register" className="link-danger">
+                    Registruj se
+                  </a>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Login
