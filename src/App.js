00298abd08c7eda5ccdcf221e68c821563fe1c7a
Tamara1406 <tamaram0053@gmail.com>
2023-06-28 21:26:17 +0200

    Kreirana Register stranica i dodata funkcionalnost

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Register from './components/Register';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <BrowserRouter className="App">
+      <Routes>
+        <Route path = "/login" element = {<Login/>} />
+        <Route path = "/register" element = {<Register/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
