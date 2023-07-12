c333544097ec4731ad8a7c686c12324e835bb29a
Lena Loncarevic <loncareviclenaa@gmail.com>
2023-07-13 01:27:08 +0200

    logout funkcionalnost

--- src/App.js
+++ src/App.js
@@ -1,63 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login';
-import Register from './components/Register';
-import NavBar from './components/NavBar';
-import Documents from './components/Documents';
-import { BrowserRouter, Route, Routes, Router } from 'react-router-dom';
-import { useState } from 'react';
-import Autors from './components/Autors';
-import AddDocument from './components/AddDocument';
-import axios from 'axios';
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import NavBar from "./components/NavBar";
+import Documents from "./components/Documents";
+import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
+import React, { useState } from "react";
+import Autors from "./components/Autors";
+import AddDocument from "./components/AddDocument";
+import axios from "axios";
 
 function App() {
-const [token, setToken] = useState();
+  // const [token, setToken] = useState(
+  //   window.sessionStorage.getItem("auth_token")
+  // );
 
-function addToken( auth_token ){
-  setToken(auth_token);
-}
+  const storedToken = window.sessionStorage.getItem("auth_token");
+  const [token, setToken] = useState(storedToken);
 
-function deleteDocument(id){
-  axios
-  .delete("api/documents/" + id,{headers:{'Authorization': `Bearer ${ window.sessionStorage.getItem('auth_token')}`} } )
-  .then((res)=>{  
-      console.log(res.data);
-      const token = window.sessionStorage.getItem('auth_token');
-      window.location. reload();
-      window.sessionStorage.set('auth_token',token);
+  function handleLogout() {
+    setToken(null);
+    window.sessionStorage.removeItem("auth_token");
+  }
 
-  })
-  .catch(function (error) {
-    if (error.response) {
-      // Request made and server responded
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message);
-    }
+  function addToken(auth_token) {
+    setToken(auth_token);
+    window.sessionStorage.setItem("auth_token", auth_token);
+  }
 
-  });
-}
-
-
+  function deleteDocument(id) {
+    axios
+      .delete("api/documents/" + id, {
+        headers: {
+          Authorization: `Bearer ${window.sessionStorage.getItem(
+            "auth_token"
+          )}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        const token = window.sessionStorage.getItem("auth_token");
+        window.location.reload();
+        window.sessionStorage.set("auth_token", token);
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // Request made and server responded
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+      });
+  }
 
   return (
     <BrowserRouter className="App">
-     
       <Routes>
-      
-        <Route path = "/" element = {<NavBar token = { token } />} >
-          <Route path = "login" element = {<Login addToken = { addToken } />} />
-          <Route path = "register" element = {<Register/>} />
-          <Route path = "documents" element = {<Documents onDelete = {deleteDocument}/>} />
-          <Route path = "autors" element = {<Autors/>} />
+        <Route
+          path="/"
+          element={<NavBar token={token} handleLogout={handleLogout} />}
+        >
+          <Route path="login" element={<Login addToken={addToken} />} />
+          <Route path="register" element={<Register />} />
+          <Route
+            path="documents"
+            element={<Documents token={token} onDelete={deleteDocument} />}
+          />
+          <Route path="autors" element={<Autors />} />
 
-          <Route path = "adddocuments" element = {<AddDocument token = { token } />} />
+          <Route path="adddocuments" element={<AddDocument token={token} />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,80 +1,78 @@
-import React from 'react'
-import axios from 'axios';
-import { Outlet } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React from "react";
+import axios from "axios";
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const NavBar = ({ token }) => {
-
-    function handleLogout(e) {
-       let config = {
-        method: 'post',
-        url: 'api/logout',
-        headers: { 
-          'Authorization': 'Bearer ' + window.sessionStorage.getItem("auth_token"),
-        }
-      };
-      
-      window.sessionStorage.setItem("auth_token", null);
-
-      axios(config)
+const NavBar = ({ token, handleLogout }) => {
+  const navigate = useNavigate();
+  function handleLogoutClick(e) {
+    e.preventDefault();
+    axios
+      .post("api/logout", null, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
       .then((response) => {
         console.log(JSON.stringify(response.data));
+        handleLogout();
+        navigate("/");
       })
       .catch((error) => {
         console.log(error);
       });
-      
-      
-    }
-    
 
+    //setToken(null);
+  }
+  console.log("token: " + token);
   return (
-<div>
-<nav className ="navbar navbar-expand-lg navbar-light bg-light">
-  <div className = "container-fluid">
-    <a className = "navbar-brand" 
-        href="#">
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
             DMS
-    </a>
-    <button className = "navbar-toggler" 
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarNavAltMarkup" 
-            aria-controls="navbarNavAltMarkup" 
-            aria-expanded="false" 
-            aria-label="Toggle navigation">
-      <span className = "navbar-toggler-icon"></span>
-    </button>
-    <div className = "collapse navbar-collapse show" 
-            id="navbarNavAltMarkup">
-      <div className = "navbar-nav">
-        <a className = "nav-link active" 
-            aria-current="page" 
-            href="/documents">
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse show"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="/documents"
+              >
                 Dokumenta
-        </a>
-        <a className = "nav-link" 
-            href="/autors">
+              </a>
+              <a className="nav-link" href="/autors">
                 Autori
-        </a>
-        { token == null ? 
-        (<a className = "nav-link" href="/login">
-                Login
-        </a>) : 
-        (<a className = "nav-link" href="/" onClick={handleLogout}>
-                Logout
-        </a>)
-        }
-        
-      </div>
+              </a>
+              {token == null ? (
+                <a className="nav-link" href="/login">
+                  Login
+                </a>
+              ) : (
+                <a className="nav-link" href="/" onClick={handleLogout}>
+                  Logout
+                </a>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
+      <Outlet />
     </div>
-  </div>
-</nav>
-<Outlet/>
-</div>
+  );
+};
 
-
-)
-}
-
-export default NavBar
+export default NavBar;
