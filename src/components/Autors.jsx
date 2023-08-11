9f3aa33cf03f8dfc6466f803f499ab56310db3a5
Lena Loncarevic <loncarevicclena@gmail.com>
2023-08-11 15:17:36 +0200

    Pretrazi autore

--- src/components/Autors.jsx
+++ src/components/Autors.jsx
@@ -8,6 +8,7 @@ import {useAuth} from "./AuthContext";
 const Autors = ({onDelete}) => {
     const [autors, setAutors] = useState();
     const {userRole} = useAuth();
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
         if (autors == null) {
@@ -26,44 +27,60 @@ const Autors = ({onDelete}) => {
         setAutors(sortedAutors); // Update the autors state with the sorted array
     };
 
-    return (<div> {
-        userRole === "admin" && (<button className="text-50 fw-bold"
-            style={
-                {
-                    backgroundColor: "#4682B4",
-                    color: "white",
-                    borderRadius: "10px",
-                    fontSize: "15px",
-                    marginLeft: "370px"
-                }
-            }
-            onClick={
-                () => navigate("/addautors")
-        }>
-            Novi autor
-        </button>)
-    }
+    return (
+        <div> {
+            userRole === "admin" && (
+                <button className="text-50 fw-bold"
+                    style={
+                        {
+                            backgroundColor: "#4682B4",
+                            color: "white",
+                            borderRadius: "10px",
+                            fontSize: "15px",
+                            marginLeft: "370px"
+                        }
+                    }
+                    onClick={
+                        () => navigate("/addautors")
+                }>
+                    Novi autor
+                </button>
+            )
+        }
 
-        <button onClick={sortAutors}
-            style={
-                {
-                    backgroundColor: "#4682B4",
-                    color: "white",
-                    borderRadius: "10px",
-                    fontSize: "15px",
-                    marginLeft: "1300px",
-                    marginTop: "100px"
+            <input type="text" placeholder="Pretrazi autore"
+                value={searchQuery}
+                onChange={
+                    (e) => setSearchQuery(e.target.value)
                 }
-        }>
-            Sort by brojDokumenata
-        </button>
-        {
-        autors == null ? (<></>) : (autors.map((autor) => <OneAutor autor={autor}
-            key={
-                autor.id
-            }
-            onDelete={onDelete}/>))
-    } </div>);
+                
+            />
+
+            <button onClick={sortAutors}
+                style={
+                    {
+                        backgroundColor: "#4682B4",
+                        color: "white",
+                        borderRadius: "10px",
+                        fontSize: "15px",
+                        marginLeft: "1300px",
+                        marginTop: "100px"
+                    }
+            }>
+                Sort by brojDokumenata
+            </button>
+            {
+            autors == null ? (
+                <></>
+            ) : ( autors
+                .filter((autor) =>
+                    autor.ime.toLowerCase().includes(searchQuery.toLowerCase())
+                )
+                .map((autor) => (
+                    <OneAutor autor={autor} key={autor.id} onDelete={onDelete} />
+                ))
+        )} </div>
+    );
 };
 
 export default Autors;
